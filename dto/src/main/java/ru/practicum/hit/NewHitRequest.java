d36208ee09ce1f92338b3d2e65eb4d563edc3601
Romanenko3490 <Saff-1@yandex.ru>
2025-10-09 11:52:50 +0300

    completed

--- dto/src/main/java/ru/practicum/hit/NewHitRequest.java
+++ dto/src/main/java/ru/practicum/hit/NewHitRequest.java
@@ -3,30 +3,37 @@ package ru.practicum.hit;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
 import org.hibernate.validator.constraints.URL;
+import org.springframework.validation.annotation.Validated;
 
 import java.time.LocalDateTime;
 
 
-@Data
+@Value // Работает так же как и @Data только создает immutable класс с final полями и без сеттеров
+@Jacksonized // @Value конфликтует с джексеном, и он не может десериальизвать
+             // LocalDateTime, нужна зависимость jackson-databind
+@Builder // @Jacksonized работает через @Buildeer
 public class NewHitRequest {
 
     @NotBlank
-    private String app;
+    String app;
 
     @NotBlank
     @URL
-    private String uri;
+    String uri;
 
     @Pattern(regexp = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.)" +
             "{3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$|^([0-9a-fA-F]{1,4}:)" +
             "{7}[0-9a-fA-F]{1,4}$",
             message = "Invalid IP address")
-    private String ip;
+    String ip;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime timestamp;
+    LocalDateTime timestamp;
 
 
 }
